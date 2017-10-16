4a825f1e74556af8c19382d817267cf4341fecdd
zhuchaoyang <zcy_2013@163.com>
2017-10-16 20:34:52 +0800

    About 组件实现及样式优化

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
+import './index.scss'
+
 class About extends Component {
   render(){
     return (
-      <div>
-        <h1>About Me~~!</h1>
+      <div className="about">
+        <h1 className="about-title">关于我</h1>
+        <div className="about-content">
+          <div className="item">
+            <div className="item-icon">
+              <i className="iconfont icon-age"></i>
+            </div>
+            <div className="item-info">
+              <p className="info-title">年龄</p>
+              <p className="info-desc">18</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
